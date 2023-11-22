840e4e9b332769c6123a4727ee9174ee1f34b709
Sripath Cherukuri <sripath.cherukuri@gmail.com>
2023-11-22 00:48:43 -0500

    updated home to 2 buttons

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 // import { Navbar, Nav } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import HomeStyles from "../styles/home.css"; // Uncomment if you have custom styles
 
 const Home = () => {
@@ -9,8 +8,9 @@ const Home = () => {
             <div className="container mt-3 title-container">
                 <h1>Welcome to the Student Survey Form Home Page!!!</h1>
                 <button type="button" class="btn btn-primary">Submit Form</button>
-                <button type="button" class="btn btn-warning">Update Records</button>
-                <button type="button" class="btn btn-danger">Delete Records</button>
+                <button type="button" class="btn btn-warning">View / Delete / Update Survey</button>
+
+                <p><b>SWE 645 - HOMEWORK ASSIGNMENT 3</b> <br></br><b>Team Members: Siva Chevuri (G), Sripath Cherukuri (G01395231), Mahitha (G), Naveen (G)</b></p>
             </div>
         </>
     );
